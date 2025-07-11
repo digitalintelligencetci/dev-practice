86994d6632f11e4abfff33f956279050f6eada01
wisestghoul89 <o.hashim3249@student.leedsbeckett.ac.uk>
2025-07-11 22:46:40 +0100

    Starting point page updated.

--- app-software-dev-portfolio/df-tool-app/app/_layout.tsx
+++ app-software-dev-portfolio/df-tool-app/app/_layout.tsx
@@ -1,29 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useEffect } from 'react'; // Importing React for useEffect hook
+import { Stack } from 'expo-router'; // Importing Stack from expo-router
+import { StatusBar } from 'expo-status-bar'; // Importing StatusBar from expo-status-bar
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+export default function RootLayout() { // Defining the RootLayout component
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name ="tabs" options ={{ headerShown: false}}></Stack.Screen>
+        <Stack.Screen name ="Page Not Found" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <StatusBar style='auto' />
+      </>
   );
 }
+
